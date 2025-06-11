9f6655e0bf93fbd8dc0c3990de7cab2763282012
ubing <ubing0101@kakao.com>
2025-06-11 10:04:37 +0900

    init

---
+++ src/main/java/com/playdata/concurrencyissues/ConcurrencyIssuesApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.concurrencyissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyIssuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyIssuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playdata/concurrencyissues/ConcurrencyIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.concurrencyissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyIssuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
